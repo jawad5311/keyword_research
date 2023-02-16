08bb7b0b92952a34b582144e4cbd9a5c7f9996e5
Jawad <muhammadjawad5311@gmail.com>
2023-02-16 15:16:30 +0500

    code converted into OOP format

--- src/keyword_research/main.py
+++ src/keyword_research/main.py
@@ -3,25 +3,57 @@
 # Selenium Imports
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 # Web driver manager imports
 from webdriver_manager.chrome import ChromeDriverManager
 
-
-# Selenium Webdriver Options settings
-chr_options = Options()
-chr_options.add_experimental_option("detach", True)
-
-# driver = webdriver.Chrome('../../chromedriver_linux64/chromedriver')
-driver = webdriver.Chrome(ChromeDriverManager().install(),
-                          options=chr_options)
-
-driver.get("https://www.google.com")
+# Builtin Modules
+import time
 
 
+class KeywordResearch():
+    def __init__(self):
+        # Downloading driver
+        self.driver_path = ChromeDriverManager().install()
+
+        # Selenium Webdriver Options settings
+        self.chr_options = Options()
+        self.chr_options.add_experimental_option("detach", True)
+        self.driver = self.start_driver()
+
+    def start_driver(self):
+        web_driver = webdriver.Chrome(self.driver_path,
+                                      options=self.chr_options)
+        return web_driver
+
+    def test_webdriver(self):
+        self.driver.get("https://www.google.com")
+        search_box = self.driver.find_element(By.CLASS_NAME, "gLFyf")
+        search_box.send_keys('test')
+
+    def do_keyword_research_on_google(self):
+        self.driver.get("https://www.google.com")
+
+        # google search box
+        search_box = self.driver.find_element(By.CLASS_NAME, "gLFyf")
+        search_box.send_keys('test')
+
+        suggested_keywords = self.driver.find_element(By.XPATH, "//div[@class='OBMEnb']/ul/li")
+        print(suggested_keywords)
+
+        for word in suggested_keywords:
+            print(word)
 
 
 
 
 
 
+
+
+if __name__ == "__main__":
+    driver = KeywordResearch()
+    # driver.test_webdriver()
+
+    driver.do_keyword_research_on_google()
